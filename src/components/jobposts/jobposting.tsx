8a1415282ecba8d414f6625df6d0d671847b4226
Lee Donson <leesdev44@gmail.com>
2025-07-17 00:58:32 +1000

    Fix state and localStorage build errors

--- src/components/jobposts/jobposting.tsx
+++ src/components/jobposts/jobposting.tsx
@@ -8,19 +8,21 @@ import { redirect } from "next/navigation";
 
 export const JobPosting = () => {
   const { data: session, status } = useSession();
-  if (!session && status === "unauthenticated") {
-    return redirect("/auth/sign-in");
-  }
-  const [preview, setPreview] = useState(false);
   const [mounted, setMounted] = useState(false);
+  const [preview, setPreview] = useState(false);
 
   useEffect(() => {
     setMounted(true);
-  }, []);
+    if (!session && status === "unauthenticated") {
+      redirect("/auth/sign-in");
+    }
+  }, [session, status]);
+
+  if (!mounted) return null;
 
   return (
     <>
-      {!mounted && !preview ? (
+      {!preview ? (
         <div className="w-full max-w-2xl mx-auto mt-10 p-5 min-h-screen flex items-center justify-center">
           <JobpostingPreview setPreview={() => setPreview(true)} />
         </div>
